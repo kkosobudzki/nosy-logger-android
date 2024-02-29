dc88ef78911acc907283035c415d90675b3ef511
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-02-29 13:18:28 +0100

    feat: add maven publish configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.protobuf)
+
+    `maven-publish`
+    signing
 }
 
 android {
@@ -91,6 +94,66 @@ protobuf {
                 create("grpc") {
                     option("lite")
                 }
+            }
+        }
+    }
+}
+
+publishing {
+    publications {
+        register("release", MavenPublication::class) {
+            groupId = "dev.nosytools"
+            artifactId = "logger"
+            version = android.defaultConfig.versionName
+
+            pom {
+                name = "logger"
+                description = "Nosy Logger android plugin"
+                url = "https://logger.nosytools.dev/"
+
+                licenses {
+                    license {
+                        name = "GNU GPL v3"
+                        url = "https://www.gnu.org/licenses/gpl-3.0.html"
+                    }
+                }
+
+                scm {
+                    url = "https://github.com/kkosobudzki/nosy-android.git"
+                }
+
+                developers {
+                    developer {
+                        name = 'Krzysztof Kosobudzki'
+                        email = 'krzysztof.kosobudzki@gmail.com'
+                        // TODO update with nosytools.dev email
+                    }
+                }
+            }
+
+            afterEvaluate {
+                from components.release
+            }
+        }
+    }
+
+    signing {
+        def id = System.getenv("SIGNING_KEY_ID")
+        def password = System.getenv("SIGNING_KEY_PASSWORD")
+        def key = System.getenv("SIGNING_KEY")
+
+        useInMemoryPgpKeys(id, key, password)
+
+        sign publishing.publications.release
+    }
+
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
             }
         }
     }
