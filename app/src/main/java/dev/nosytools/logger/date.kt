ae55e3f32cbd49ecd2fdb1f3ae9ca5fcad063de9
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-01 15:59:55 +0100

    feat: use predefined iso formatter

--- app/src/main/java/dev/nosytools/logger/date.kt
+++ app/src/main/java/dev/nosytools/logger/date.kt
@@ -1,7 +1,5 @@
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-
 internal fun now(): String =
-    LocalDateTime.now().format(formatter)
+    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
