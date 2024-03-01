59d5ffa8dded50a68cf85beb28eca37d2c97d36f
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-01 11:12:31 +0100

    feat(bc): init bc provider in logger

--- app/src/main/java/dev/nosytools/logger/Logger.kt
+++ app/src/main/java/dev/nosytools/logger/Logger.kt
@@ -10,62 +10,72 @@ import nosy_logger.LoggerGrpc
 import nosy_logger.LoggerGrpc.LoggerStub
 import nosy_logger.LoggerOuterClass.Empty
 import nosy_logger.LoggerOuterClass.Log
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.Security
 
 class Logger(private val config: Config) {
 
-  private val stub: LoggerStub by lazy {
-    val headers = Metadata().apply {
-      put(API_KEY_METADATA, config.apiKey)
+    init {
+        Security.removeProvider("BC");
+        Security.addProvider(BouncyCastleProvider())
     }
 
-    ManagedChannelBuilder.forTarget(config.url)
-      .useTransportSecurity()
-      .build()
-      .let(LoggerGrpc::newStub)
-      .withInterceptors(MetadataUtils.newAttachHeadersInterceptor(headers))
-  }
+    private val stub: LoggerStub by lazy {
+        val headers = Metadata().apply {
+            put(API_KEY_METADATA, config.apiKey)
+        }
 
-  private val diffieHellman by lazy { DiffieHellman() }
+        ManagedChannelBuilder.forTarget(config.url)
+            .useTransportSecurity()
+            .build()
+            .let(LoggerGrpc::newStub)
+            .withInterceptors(MetadataUtils.newAttachHeadersInterceptor(headers))
+    }
 
-  private lateinit var encryptor: Encryptor
+    private val diffieHellman by lazy { DiffieHellman() }
 
-  fun init(onCompleted: () -> Unit, onError: (Throwable?) -> Unit) {
-    stub.handshake(
-      Empty.newBuilder().build(),
-      DelegatedStreamObserver(
-        whenNext = { remotePublicKey ->
-          encryptor = Encryptor(
-            sharedSecretKey = diffieHellman.sharedSecret(remotePublicKey.key)
-          )
+    private lateinit var encryptor: Encryptor
 
-          onCompleted()
-        },
-        whenError = onError
-      )
-    )
-  }
+    fun init(onCompleted: () -> Unit, onError: (Throwable?) -> Unit) {
+        stub.handshake(
+            Empty.newBuilder().build(),
+            DelegatedStreamObserver(
+                whenNext = { remotePublicKey ->
+                    encryptor = Encryptor(
+                        sharedSecretKey = diffieHellman.sharedSecret(remotePublicKey.key)
+                    )
 
-  fun log(logs: List<Log>, onCompleted: () -> Unit, onError: (Throwable?) -> Unit) {
-    logs.map(::encrypt)
-      .toLogs()
-      .also {
-        stub.log(it, DelegatedStreamObserver(whenCompleted = onCompleted, whenError = onError))
-      }
-  }
+                    onCompleted()
+                },
+                whenError = onError
+            )
+        )
+    }
 
-  private fun encrypt(log: Log): Log =
-    Log.newBuilder()
-      .setDate(log.date)
-      .setLevel(log.level)
-      .setMessage(log.message.encrypt())
-      .setPublicKey(diffieHellman.publicKey)
-      .build()
+    fun log(logs: List<Log>, onCompleted: () -> Unit, onError: (Throwable?) -> Unit) {
+        logs.map(::encrypt)
+            .toLogs()
+            .also {
+                stub.log(
+                    it,
+                    DelegatedStreamObserver(whenCompleted = onCompleted, whenError = onError)
+                )
+            }
+    }
 
-  private fun String.encrypt(): String =
-    encryptor.encrypt(this)
+    private fun encrypt(log: Log): Log =
+        Log.newBuilder()
+            .setDate(log.date)
+            .setLevel(log.level)
+            .setMessage(log.message.encrypt())
+            .setPublicKey(diffieHellman.publicKey)
+            .build()
 
-  private companion object {
-    val API_KEY_METADATA: Metadata.Key<String> =
-      Metadata.Key.of("api-key", Metadata.ASCII_STRING_MARSHALLER)
-  }
+    private fun String.encrypt(): String =
+        encryptor.encrypt(this)
+
+    private companion object {
+        val API_KEY_METADATA: Metadata.Key<String> =
+            Metadata.Key.of("api-key", Metadata.ASCII_STRING_MARSHALLER)
+    }
 }
